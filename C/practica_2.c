d688dc24f6c1da8708c6dc573771ab5aa871fa02
Villena <villena2311@gmail.com>
2025-11-03 10:57:03 +0100

    practica 2 completa

--- C/practica_2.c
+++ C/practica_2.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
-#include <math.h>
 
-int main(){
-    int x;
-    int y;
-    int suma;
-    int resta;
-    int multiplicacion;
-    int division;
-    char op;
+int main() {
+    int x, y;
+    int suma, resta, multiplicacion;
+    float division;
 
-    printf("Por favor, introduzca el primer numero: ");
+    printf("Introduce su primer número: ");
     scanf("%d", &x);
-    printf("Por favor, introduzca su segundo numero: ");
+
+    printf("Introduce su segundo número: ");
     scanf("%d", &y);
-    printf("Por favor, decida la operacion a realizar: ");
-    scanf("%s", &op);
-    if (op == "suma"){
-        suma = x + y;
-        printf(suma);
+
+    suma = x + y;
+    resta = x - y;
+    multiplicacion = x * y;
+    division = x / y;
+
+    printf("La suma es: %d\n", suma);
+    printf("La resta es: %d\n", resta);
+    printf("La multiplicación es: %d\n", multiplicacion);
+    if (y == 0) {
+        printf("no se puede dividir entre 0\n");
+    } 
+    else {
+        division = (float)x / y; 
+        printf("La división es: %.1f\n", division);
     }
-    
-    
+
     return 0;
 }
