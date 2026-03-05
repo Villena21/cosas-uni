0085d6f40a0f238b23bceea5477a80d52f51352a
Villena <villena2311@gmail.com>
2026-03-05 11:26:04 +0100

    añadida condicion para tanto a como b

--- Python/ej_2.py
+++ Python/ej_2.py
@@ -3,15 +3,20 @@ print('Programa para la resolucion de la ecuacion ax+b=0')
 a = float(input('Insertar valor de a: '))
 b = float(input('Insertar valor de b: '))
 
+while a==0 and b==0:
+    if a==0 and b==0:
+        print('No tiene solucion')
+        a = float(input('Insertar valor de a: '))
+        b = float(input('Insertar valor de b: '))       
 while a == 0:
     if a == 0:
-        print('0 no sirve como valor de a')
-        a = float(input('Inserte valor de a: '))
+        print('No tiene solucion')
+        a = float(input('Inserte otro valor de a: '))
         b = b
 while b == 0:
     if b == 0:
-        print('0 no sirve como valor de b')
-        b=float(input('Inserte valor de b: '))
+        print('Infinitas soluciones')
+        b=float(input('Inserte otro valor de b: '))
         a=a
 
 x = -b/a
