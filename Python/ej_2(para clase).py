035a8c767f896a1258b6d204916f3d76693f3e41
Villena <villena2311@gmail.com>
2026-03-05 11:26:13 +0100

    para seguir la clase :v

---
+++ Python/ej_2(para clase).py
@@ -0,0 +1,15 @@
+print('Programa para la resolucion de la ecuacion ax+b=0')
+
+a = float(input('Insertar valor de a: '))
+b = float(input('Insertar valor de b: '))
+
+if a!=0:
+    x = -b/a
+    print('solucion', x)
+else:
+    print('No hay solucion')
+if b!=0:
+    x = -b/a
+    print('solucion', x)
+else:
+    print('No hay solucion')
