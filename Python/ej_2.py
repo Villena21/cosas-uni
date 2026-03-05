099c7e50ba0927e3013f70baa1e5b7d47541f455
Villena <villena2311@gmail.com>
2026-03-05 11:00:52 +0100

    ej 2 hecho :3

---
+++ Python/ej_2.py
@@ -0,0 +1,21 @@
+print('Programa para la resolucion de la ecuacion ax+b=0')
+
+a = float(input('Insertar valor de a: '))
+b = float(input('Insertar valor de b: '))
+
+while a == 0:
+    if a == 0:
+        print('0 no sirve como valor de a')
+        a = float(input('Inserte valor de a: '))
+        b = b
+while b == 0:
+    if b == 0:
+        print('0 no sirve como valor de b')
+        b=float(input('Inserte valor de b: '))
+        a=a
+
+print(a,'x+',b)
+
+x = -b/a
+
+print('La solucion es:', x)
